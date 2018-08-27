eefa16eb0130fcf8ed659cd4d54f17757bd53932
Zhewei Wu <zhewei.wu@libertymutual.com>
2018-08-27 11:45:58 -0400

    module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,18 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {incorrectAnswer : false}
+    }
+
     handleClick(buttonText) {
         if (buttonText===this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+            this.setState({incorrectAnswer: false})
+        }
+        else {
+            this.setState({incorrectAnswer: true})
         }
     }
 
@@ -22,6 +31,7 @@ class QuizQuestion extends Component {
                     })}
                 </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
         </main>
         )
     }
